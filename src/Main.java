b600b12f9b1828ad7faee4c411f897a777f4547e
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-23 22:59:22 +0100

    Comprobación

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Singleton primeraInstancia = Singleton.getInsatance();
 
+        primeraInstancia.setNombre("Primera instancia");
+        primeraInstancia.setEdad(0);
 
+        Singleton segundaInstancia = Singleton.getInsatance();
+
+        segundaInstancia.setEdad(1);
+
+        System.out.println(primeraInstancia.toString()); // Mirar puntero
+        System.out.println(segundaInstancia.toString()); // Mirar puntero
 
     }
 }
